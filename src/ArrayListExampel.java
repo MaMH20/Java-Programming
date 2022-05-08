619acf5cadf2238da568f6eff7696be917e26ffa
Mo_09 <mo_09@pop-os.localdomain>
2022-05-08 20:09:36 +0200

    ArryList Example

---
+++ src/ArrayListExampel.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+
+public class ArrayListExampel{
+    public static void main(String[] args){
+
+        //ArrayListExampel = a resizeble array
+
+
+        /*ArrayList<String> food = new ArrayList<String>();
+
+        food.add("Pizza");
+        food.add("Reis");
+        food.add("Hamburger");
+
+        food.set(2, "hotdog");
+        food.remove(2);
+        food.clear();
+
+        for(int i = 0; i < food.size(); i++) {
+
+            System.out.println(food.get(i));
+
+        }*/
+
+          ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(11);
+        numbers.add(22);
+        numbers.add(33);
+        numbers.add(44);
+
+        numbers.set(2, 55);
+        numbers.remove(3);
+        numbers.clear();
+
+
+        for(int i = 0; i < numbers.size(); i++) {
+
+          System.out.println(numbers.get(i));
+
+        }
+
+    }
+}
+
